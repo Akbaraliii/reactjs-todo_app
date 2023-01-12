0aa420465ea0ab45dfc88f55064ae54dc4c8aff0
Akbaraliii <91048215+Akbaraliii@users.noreply.github.com>
2023-01-12 23:09:25 +0900

    Designing layout

--- src/App.js
+++ src/App.js
@@ -1,7 +1,21 @@
 function App() {
   return (
-    <div className='App'>
-      <h1>hope</h1>
+    <div className='todo-master'>
+      <nav className='navbar app'>App bar</nav>
+      <nav className='navbar board'> Board bar</nav>
+      <div className='board-columns'>
+        <header>Brainstorm</header>
+        <ul>
+          <li>
+            <img
+              src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png'
+              alt=''
+            />
+          </li>
+          <li>second</li>
+        </ul>
+        <footer>Add another card</footer>
+      </div>
     </div>
   );
 }
