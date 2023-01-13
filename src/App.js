688377e5c179acb1bb2f4b4c3c56d7068784da76
Akbaraliii <91048215+Akbaraliii@users.noreply.github.com>
2023-01-13 10:53:24 +0900

    desinging navbar,columns, scroller,

--- src/App.js
+++ src/App.js
@@ -4,17 +4,101 @@ function App() {
       <nav className='navbar app'>App bar</nav>
       <nav className='navbar board'> Board bar</nav>
       <div className='board-columns'>
-        <header>Brainstorm</header>
-        <ul>
-          <li>
-            <img
-              src='https://raw.githubusercontent.com/haryphamdev/sharing-host-files/master/trello/img-design.png'
-              alt=''
-            />
-          </li>
-          <li>second</li>
-        </ul>
-        <footer>Add another card</footer>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src='https://raw.githubusercontent.com/Akbaraliii/reactjs-todo_app/master/public/img-design.png'
+                alt='Design and reasearch'
+              />
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>1</li>
+            <li>1</li>
+            <li>second</li>
+            <li>1</li>
+            <li>1</li>
+            <li>second</li>
+            <li>1</li>
+            <li>1</li>
+            <li>1</li>
+            <li>second</li>
+            <li>1</li>
+            <li>1</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src='https://raw.githubusercontent.com/Akbaraliii/reactjs-todo_app/master/public/img-design.png'
+                alt='Design and reasearch'
+              />
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src='https://raw.githubusercontent.com/Akbaraliii/reactjs-todo_app/master/public/img-design.png'
+                alt='Design and reasearch'
+              />
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>1</li>
+            <li>1</li>
+            <li>second</li>
+            <li>1</li>
+            <li>1</li>
+            <li>second</li>
+            <li>1</li>
+            <li>1</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
+        <div className='column'>
+          <header>Brainstorm</header>
+          <ul>
+            <li>
+              <img
+                src='https://raw.githubusercontent.com/Akbaraliii/reactjs-todo_app/master/public/img-design.png'
+                alt='Design and reasearch'
+              />
+            </li>
+            <li>second</li>
+            <li>third</li>
+            <li>second</li>
+            <li>1</li>
+            <li>1</li>
+            <li>second</li>
+            <li>1</li>
+            <li>1</li>
+            <li>second</li>
+            <li>1</li>
+            <li>1</li>
+            <li>second</li>
+            <li>1</li>
+            <li>1</li>
+            <li>second</li>
+            <li>1</li>
+            <li>1</li>
+          </ul>
+          <footer>Add another card</footer>
+        </div>
       </div>
     </div>
   );
